f9c5e876e092aa1411ece239e946c99dac68ad0a
macphisto82 <alessandro.pilonato@gmail.com>
2019-01-12 15:28:53 +0100

    Bug fixing, di assignment

--- src/main/java/guru/springframework/services/PrimarySpanishGreetingService.java
+++ src/main/java/guru/springframework/services/PrimarySpanishGreetingService.java
@@ -13,8 +13,12 @@ import org.springframework.stereotype.Service;
 public class PrimarySpanishGreetingService implements GreetingService {
 
     private GreetingRepository greetingRepository;
+    
+    public PrimarySpanishGreetingService(GreetingRepository greetingRepository) {
+		this.greetingRepository = greetingRepository;
+	}
 
-    @Override
+	@Override
     public String sayGreeting() {
         return greetingRepository.getSpanishGreeting();
     }
